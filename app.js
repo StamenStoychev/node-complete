ea16db559524b1c369e0f6c9772c4b9597dd9602
Stamen Stoychev <sstoychev98@gmail.com>
2020-09-17 22:05:59 +0300

    MongoDB starting

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 const pageNotFoundController = require("./controllers/404");
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -23,14 +16,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // adding the user info to the request
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
 app.use("/admin", adminRoutes.routes);
@@ -39,39 +32,4 @@ app.use(shopRoute);
 
 app.use(pageNotFoundController.get404);
 
-//Association from Prods to User
-// when we delete the user, all the products related to him will be deleted
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
 
-sequelize
-  //force - to overwrite the tables
-  // .sync({ force: true })
-  .sync()
-  .then((res) => {
-    //logging the current user
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Stamen", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // after we create a user, now create a cart
-    user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
