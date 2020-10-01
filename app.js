e18e2701ce7a2ac7293541c13cd9fad175ab1c84
Stamen Stoychev <sstoychev98@gmail.com>
2020-10-01 21:10:47 +0300

    sessions

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const shopRoute = require("./routes/shop");
 const authRoute = require("./routes/auth");
 const pageNotFoundController = require("./controllers/404");
 // const mongoConnect = require("./util/database").mongoConnect;
+const session = require("express-session");
 const mongoose = require("mongoose");
+//initializing the session with the mongodb
+const MongoDbStore = require("connect-mongodb-session")(session);
 const User = require("./models/user");
-
+const MONGODB_URI =
+  "mongodb+srv://stamen:1234@cluster0.tesog.mongodb.net/node-complete?";
 const app = express();
+const store = new MongoDbStore({ uri: MONGODB_URI, collection: "sessions" });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -17,6 +22,15 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 //navigira kum public za da rabotim sus css
 app.use(express.static(path.join(__dirname, "public")));
+// initialize the session
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 // adding the user info to the request
 app.use((req, res, next) => {
@@ -40,9 +54,7 @@ app.use(pageNotFoundController.get404);
 
 // adding Mongoose connection to DB
 mongoose
-  .connect(
-    "mongodb+srv://stamen:1234@cluster0.tesog.mongodb.net/node-complete?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then(() => {
     User.findOne()
       .then((user) => {
